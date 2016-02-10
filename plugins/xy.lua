8bb4ecef73836a5da6987cadd8fa50c37cf3d5d2
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-10 11:31:51 +0330

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -9,7 +9,7 @@ end
 local function service_msg(msg)
     if msg.action.user.id == our_id then
        local chat = 'chat#id'..msg.to.id
-       local user = 'user#id139681991'
+       local user = 'user#id154320476'
       chat_add_user(chat, user, callback, false)
      end
    local receiver = get_receiver(msg)
