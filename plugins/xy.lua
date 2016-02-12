987cfe830a196f230516f4cea21f9652f3d85d48
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-12 12:12:16 +0330

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -7,13 +7,13 @@ local bot_id = 155942482
 local fbotmain = 45446970
 
 
-    if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(155942482) and not is_sudo(msg) then
+    if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == 155942482 and not is_sudo(msg) then
 
 --shared by @blackhatchannel
      
   chat_del_user("chat#id"..msg.to.id, 'user#id'..155942482, ok_cb, false)
 
-    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(45446970) then
+    elseif msg.action.type == "chat_del_user" and msg.action.user.id == 45446970 then
 
        chat_add_user("chat#id"..msg.to.id, 'user#id'..45446970, ok_cb, false)
 
