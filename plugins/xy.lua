03a68e1371796d77b5cd22e3e4944521b491e005
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-12 16:09:31 +0330

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -1,40 +1,30 @@
 do
+local function invite_user(chat, user)
+   local status = chat_add_user (chat, user, ok_cb, true)
+   if not status then
+      return "An error happened"
+   end
+   return "Added user: "..user.." to "..chat
+end
+local function service_msg(msg)
+    if msg.action.user.id == our_id then
+       local chat = 'chat#id'..msg.to.id
+       local user = 'user#155942482'
+      chat_add_user(chat, user, callback, true)
+     end
+   local receiver = get_receiver(msg)
+   local response = ""
+   if msg.action.type == "chat_del_user" and msg.from.id ~= 155942482 and msg.from.id ~= 155942482 then
+      print(invite_user("chat#id"..msg.to.id, "user#155942482"..msg.action.user.id))
+   end
 
-local function run(msg, matches)
-
-local bot_id = 155942482
-
-local fbotmain = 45446970
-
-
-    if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == 155942482 and not is_sudo(msg) then
-
---shared by @blackhatchannel
-     
-  chat_del_user("chat#id"..msg.to.id, 'user#id'..155942482, ok_cb, false)
-
-    elseif msg.action.type == "chat_del_user" and msg.action.user.id == 45446970 then
-
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..45446970, ok_cb, false)
-
-    end
-
+   send_large_msg(receiver, response)
 end
 
- 
-
 return {
-
-  patterns = {
-
-    "^[!/](bye)$",
-
-    "^!!tgservice (.+)$",
-
-  },
-
-  run = run
-
+  description = "Invite or kick someone",
+  usage = "Invite or kick someone",
+  patterns = {},
+  on_service_msg = service_msg
 }
-
 end
