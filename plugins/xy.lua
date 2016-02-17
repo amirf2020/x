aa194474ef76c257528bed5b81d0823db91c6338
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-17 08:57:05 +0330

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -5,10 +5,8 @@ local y = 155942482
 local z = 136378403
     if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
-    end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(z) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..z, ok_cb, false)
-    end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(y) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..y, ok_cb, false)
     end
