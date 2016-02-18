c1f03e32d42c1d5f8940b61e9861ec24f1a21c97
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-18 18:40:24 +0330

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -20,46 +20,101 @@ local n = 181931593
 local o = 181931593
 local p = 191871309
 local q = 172916552
+ if msg.text == "xy on" or msg.text == "Xy on" then
+ if is_sudo(msg) then
+    xy = 1
+    return "on✔"
+ else
+ return "این دستور فقط برای سودو است"
+ end
+ elseif msg.test == "xy off" or msg.text == "Xy off" then
+ if is_sudo(msg) then
+    xy = 0
+    return "off❌"
+ else
+ return "این دستور فقط برای سودو است"
+ end
+ end
     if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(z) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..z, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(y) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..y, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(a) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..a, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(b) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..a, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(c) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..c, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(d) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..d, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(e) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..e, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(f) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..f, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(g) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..g, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(h) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..h, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(i) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..i, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(j) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..j, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(k) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..k, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(l) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..l, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(m) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..m, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(n) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..n, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(o) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..o, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(p) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..p, ok_cb, false)
+end
   elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(q) then
+    if tonumber(xy) == 1 then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..q, ok_cb, false)
+end
     end
 end
  
